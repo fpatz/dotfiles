e98aef1b96af800ff6b3b48eb8c5f8d502360078
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2022-12-28 17:48:01 +0100

    add init.el

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,6 @@
+;; This is init.el, and it is generated from emacs.org!
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.9)
+(org-babel-load-file "~/.emacs.d/emacs.org")
+(setq gc-cons-threshold 16777216
+      gc-cons-percentage 0.1)
