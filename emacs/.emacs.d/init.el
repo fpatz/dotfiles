6a141c318fd14979870e248281ad473f6d6dcf59
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2022-12-28 19:03:57 +0100

    using straight

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,19 @@
 ;; This is init.el, and it is generated from emacs.org!
+
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.9)
 (org-babel-load-file "~/.emacs.d/emacs.org")
